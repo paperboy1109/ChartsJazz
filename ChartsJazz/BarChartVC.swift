02ed4e78a3fda708edd4c36abda91d9fa2255d63
Daniel Janiak <danieljjaniak@icloud.com>
2016-08-23 12:46:24 -0700

    Initial commit (Pods installed)

---
+++ ChartsJazz/BarChartVC.swift
@@ -0,0 +1,31 @@
+//
+//  BarChartVC.swift
+//  ChartsJazz
+//
+//  Created by Daniel J Janiak on 8/23/16.
+//  Copyright © 2016 Daniel J Janiak. All rights reserved.
+//
+
+import UIKit
+
+class BarChartVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
