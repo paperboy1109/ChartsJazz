acb6af84e41c3048d875e2bf9e224629aed3e503
Daniel Janiak <danieljjaniak@icloud.com>
2016-08-23 14:04:09 -0700

    Create another scene with a pie chart and a line plot; embed all (two) scenes in a tab bar

---
+++ ChartsJazz/LineAndPieVC.swift
@@ -0,0 +1,68 @@
+//
+//  LineAndPieVC.swift
+//  ChartsJazz
+//
+//  Created by Daniel J Janiak on 8/23/16.
+//  Copyright © 2016 Daniel J Janiak. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class LineAndPieVC: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var lineChartView: LineChartView!
+    @IBOutlet weak var pieChartView: PieChartView!    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /* Create some sample data */
+        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0]
+        
+        setChart(months, values: unitsSold)
+    }
+    
+    // MARK: - Helpers
+    
+    func setChart(dataPoints: [String], values: [Double]) {
+        
+        var dataEntries: [ChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        let pieChartDataSet = PieChartDataSet(yVals: dataEntries, label: "Units Sold")
+        let pieChartData = PieChartData(xVals: dataPoints, dataSet: pieChartDataSet)
+        pieChartView.data = pieChartData
+        
+        var colors: [UIColor] = []
+        
+        for _ in 0..<dataPoints.count {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+        }
+        
+        pieChartDataSet.colors = colors
+        
+        
+        let lineChartDataSet = LineChartDataSet(yVals: dataEntries, label: "Units Sold")
+        let lineChartData = LineChartData(xVals: dataPoints, dataSet: lineChartDataSet)
+        lineChartView.data = lineChartData
+        
+    }
+    
+    
+    
+}
